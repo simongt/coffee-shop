8a249f7f08571bff2d064e18a0c98ea003194add
Sy <simon.tsegay@gmail.com>
2020-09-28 16:58:06 -0400

    add menu constants

---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+import {v4 as uuidv4} from 'uuid';
+
+export const MENU = [
+  {
+    id: uuidv4(),
+    name: `Café au lait`,
+    duration: 4,
+  },
+  {
+    id: uuidv4(),
+    name: 'Cappuccino',
+    duration: 10,
+  },
+  {
+    id: uuidv4(),
+    name: 'Espresso',
+    duration: 15,
+  },
+];
