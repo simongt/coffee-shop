03f035d1acc18b1eaacfd04c69c3b284e0d08013
Sy <simon.tsegay@gmail.com>
2020-09-28 19:42:53 -0400

    add screen width / height to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,8 @@
+import {Dimensions} from 'react-native';
+
+export const WINDOW_WIDTH = Dimensions.get('window').width;
+export const WINDOW_HEIGHT = Dimensions.get('window').height;
+
 export const MENU = [
   {
     id: 1,
