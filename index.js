1ae03f8ffcbac1719c3de574e1f3d6422858fdb0
Sy <simon.tsegay@gmail.com>
2020-09-29 05:59:37 -0400

    add ignore-log for warning

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-import {AppRegistry} from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
 import 'react-native-get-random-values';
 import App from './App';
 import {name as appName} from './app.json';
+
+LogBox.ignoreLogs([
+  'Cannot update a component from inside the function body of a different component.',
+]);
 
 AppRegistry.registerComponent(appName, () => App);
